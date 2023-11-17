66f5b77f68933f0b1993c69e72086393124e34c6
oireland <irelandoliver4@gmail.com>
2023-11-17 21:38:51 +0000

    logging NEXT_PUBLIC_SITE_URL and VERCEL_URL

--- lib/getURL.ts
+++ lib/getURL.ts
@@ -1,6 +1,7 @@
 const IS_SERVER = typeof window === "undefined";
 export default function getURL(path: string) {
   console.log(process.env.NEXT_PUBLIC_SITE_URL)
+  console.log(process.env.VERCEL_URL)
   const baseURL = IS_SERVER
     ? process.env.NEXT_PUBLIC_SITE_URL
     : window.location.origin;
