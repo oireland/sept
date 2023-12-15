6762092d8d9729c42e287ae50598ce79bd34bb3a
oireland <irelandoliver4@gmail.com>
2023-12-15 12:10:34 +0000

    style: legend added to scoreboard and x-axis labels removed

--- app/(allUsers)/dashboard/Scoreboard.tsx
+++ app/(allUsers)/dashboard/Scoreboard.tsx
@@ -5,6 +5,7 @@ import {
   BarChart,
   CartesianGrid,
   Cell,
+  Legend,
   ResponsiveContainer,
   Tooltip,
   XAxis,
@@ -25,18 +26,45 @@ export default function Scoreboard({ data }: { data: ScoreboardData }) {
         <XAxis
           dataKey="teamName"
           stroke="#888888"
-          className="text-[8px] sm:text-sm md:text-base lg:text-lg"
+          className="hidden"
           axisLine={false}
           tickLine={false}
           type="category"
         />
-        <YAxis stroke="#888888" fontSize={12} axisLine={false} type="number" />
+        <YAxis
+          stroke="#888888"
+          fontSize={12}
+          axisLine={false}
+          type="number"
+          width={10}
+        />
         <Tooltip />
-        <Bar dataKey="points" fill="#004225" radius={[4, 4, 0, 0]}>
+        <Bar dataKey="points" radius={[4, 4, 0, 0]}>
           {data.map(({ teamColour }, index) => (
             <Cell key={index} fill={teamColour} />
           ))}
         </Bar>
+        <Legend
+          content={() => {
+            return (
+              <ul className="grid grid-cols-2 sm:flex w-full justify-evenly">
+                {data.map(({ teamColour, teamName }) => (
+                  <li key={teamName}>
+                    <div className="flex space-x-2 items-center">
+                      <div
+                        className="h-3 w-3 sm:h-4 sm:w-4 lg:w-6 lg:h-6 xl:w-7 xl:h-7 rounded-sm border"
+                        style={{ backgroundColor: teamColour }}
+                      ></div>
+                      <span className="text-xs sm:text-sm lg:text-base xl:text-lg">
+                        {teamName}
+                      </span>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            );
+          }}
+        />
       </BarChart>
     </ResponsiveContainer>
   );
